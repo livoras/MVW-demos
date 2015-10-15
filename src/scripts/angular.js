e3ceee1fec285fee81007d94eecdcf2e5f652e73
livoras <livoras@163.com>
2015-10-15 23:35:14 +0800

    add angularjs

---
+++ src/scripts/angular.js
@@ -0,0 +1,19 @@
+var angular = require("angular")
+
+angular.module("todoApp", [])
+
+.controller("TodoAppController", ["$scope", function($scope) {
+  $scope.newTodoContent = "Nothing"
+  $scope.todos = [{content: "Make PPT!", done: false}]
+
+  $scope.addNewTodo = function() {
+    if ($scope.newTodoContent.length === 0) return
+    $scope.todos.push({content: $scope.newTodoContent, done: false})
+    $scope.newTodoContent = ""
+  }
+
+  $scope.checkTodo = function(i) {
+    $scope.todos[i].done = !$scope.todos[i].done
+  }
+  
+}])
